c3053b2536aa91ff7213044b89f0d17ce542c0c3
penkfreshman <1561435601@qq.com>
2022-02-28 23:04:06 +0800

    add lib

---
+++ src/main/java/com/loading/load.java
@@ -0,0 +1,12 @@
+package com.loading;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+*Application
+ */
+@SpringBootApplication
+public class load {
+    public static void main(String[] args) {
+
+    }
+}
