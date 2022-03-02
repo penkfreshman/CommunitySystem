5c1708ff879c8f4f6f27dabb570ed1cfe12b3892
penkfreshman <1561435601@qq.com>
2022-03-03 00:20:17 +0800

    UI1

--- src/main/java/com/loading/load.java
+++ src/main/java/com/loading/load.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class load {
     public static void main(String[] args) {
-
+            SpringBootApplication.run();
+            
     }
 }
