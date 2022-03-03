ccab4d0c9d5e6a80bc01b48e3b941146ab29c12f
penkfreshman <1561435601@qq.com>
2022-03-03 23:43:07 +0800

    test1

---
+++ src/test/java/com/community/system/System1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class System1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
